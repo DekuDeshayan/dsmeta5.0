72dad2fe01e9e60b18ea0d38059ca8873a8d0667
DekuDeshayan <buraimoRalph129@gmail.com>
2022-07-15 15:40:52 +0200

    Api test

---
+++ backand/src/main/java/com/nanotechmoz/dsmeta/controllers/SaleController.java
@@ -0,0 +1,28 @@
+package com.nanotechmoz.dsmeta.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nanotechmoz.dsmeta.entities.Sale;
+import com.nanotechmoz.dsmeta.services.SaleService;
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+	
+	@Autowired
+	private SaleService service;
+	
+	
+	@GetMapping
+	public List<Sale> findSales(){
+		
+		return service.findSales();
+		
+	}
+
+}
